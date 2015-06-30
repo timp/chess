1494355cec457dfd8c9d617ed731a291e5d5f33d
timp21337 <timp21337@paneris.org>
2015-06-30 08:19:08 +0100

    Extract sources from given jar

---
+++ src/com/siteintel/progtest/input/UserInput.java
@@ -0,0 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+//  Derived from com.siteintel.progtest.input.UserInput
+//
+
+package com.siteintel.progtest.input;
+
+import com.siteintel.progtest.input.UserInputException;
+
+public interface UserInput {
+    boolean nextMove(int[] var1, int[] var2) throws UserInputException;
+}
---
+++ src/com/siteintel/progtest/input/UserInputException.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+//  Derived from com.siteintel.progtest.input.UserInputException
+//
+package com.siteintel.progtest.input;
+
+public class UserInputException extends Exception {
+    public UserInputException(String reason) {
+        super(reason);
+    }
+
+    public UserInputException(Throwable e) {
+        super(e);
+    }
+}
