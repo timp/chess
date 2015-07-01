39e7d36a2c8c1dbc385d053255058c14d8253c82
timp21337 <timp21337@paneris.org>
2015-07-01 08:59:42 +0100

    Revert

---
+++ src/toy/chess/InvalidChessCoordinate.java
@@ -0,0 +1,10 @@
+package toy.chess;
+
+/**
+ * @author timp
+ * @since 2015-07-01.
+ */
+public class InvalidChessCoordinate extends Throwable {
+  public InvalidChessCoordinate(int coord) {
+  }
+}
